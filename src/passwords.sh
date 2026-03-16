e58ce26f59f2289cc5bdc9e00b2e81f39a5b9d1a
huok <77536370+vraestoren@users.noreply.github.com>
2026-03-16 14:23:16 +0300

    Update passwords.sh

--- src/passwords.sh
+++ src/passwords.sh
@@ -1,69 +1,50 @@
 #!/bin/bash
 
 api="https://password-prod-api.nbalin.dev/v1"
-sign=null
-vk_user_id=null
-vk_ts=null
-vk_ref=null
+params=null
 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
 
+function _post() {
+    curl --request POST \
+        --url "$api/$1" \
+        --user-agent "$user_agent" \
+        --header "content-type: application/json" \
+        --header "authorization: Bearer $params" \
+        ${2:+--data "$2"}
+}
+
+# 1 - sign: (string): <sign>
+# 2 - vk_user_id: (integer): <vk_user_id>
+# 3 - vk_ts: (integer): <vk_ts>
+# 4 - vk_ref: (string): <vk_ref>
+# 5 - access_token_settings: (string): <access_token_settings - default: >
+# 6 - are_notifications_enabled: (integer): <are_notifications_enabled - default: 0>
+# 7 - is_app_user: (integer): <is_app_user - default: 0>
+# 8 - is_favorite: (integer): <is_favorite - default: 0>
+# 9 - language: (string): <language - default: ru>
+# 10 - platform: (string): <platform - default: desktop_web>
 function authenticate() {
-	# 1 - sign: (string): <sign>
-	# 2 - vk_user_id: (integer): <vk_user_id>
-	# 3 - vk_ts: (integer): <vk_ts>
-	# 4 - vk_ref: (string): <vk_ref>
-	# 5 - access_token_settings: (string): <access_token_settings - default: >
-	# 6 - are_notifications_enabled: (integer): <are_notifications_enabled: default: 0>
-	# 7 - is_app_user: (integer): <is_app_user - default: 0>
-	# 8 - is_favorite: (integer): <is_favorite - default: 0>
-	# 9 - language: (string): <language - default: ru>
-	# 10 - platform: (string): <platform - default: desktop_web>
-	sign=$1
-	vk_user_id=$2
-	vk_ts=$3
-	vk_ref=$4
-	params="vk_access_token_settings=${5:-}&vk_app_id=7908538&vk_are_notifications_enabled=${6:-0}&vk_is_app_user=${7:-0}&vk_is_favorite=${8:-0}&vk_language=${9:-ru}&vk_platform=${10:-desktop_web}&vk_ref=$vk_ref&vk_ts=$vk_ts&vk_user_id=$vk_user_id&sign=$sign"
-	echo $params
+    params="vk_access_token_settings=${5:-}&vk_app_id=7908538&vk_are_notifications_enabled=${6:-0}&vk_is_app_user=${7:-0}&vk_is_favorite=${8:-0}&vk_language=${9:-ru}&vk_platform=${10:-desktop_web}&vk_ref=$4&vk_ts=$3&vk_user_id=$2&sign=$1"
+    echo "$params"
 }
 
 function get_init() {
-	curl --request POST \
-		--url "$api/init" \
-		--user-agent "$user_agent" \
-		--header "content-type: application/json" \
-		--header "authorization: Bearer $params"
+    _post "init"
 }
 
 function get_profile() {
-	curl --request POST \
-		--url "$api/getProfile" \
-		--user-agent "$user_agent" \
-		--header "content-type: application/json" \
-		--header "authorization: Bearer $params"
+    _post "getProfile"
 }
 
+# 1 - code: (integer): <code - default: 1>
 function get_passwords() {
-	# 1 - code: (integer): <code - default: 1>
-	curl --request POST \
-		--url "$api/getPasswords" \
-		--user-agent "$user_agent" \
-		--header "content-type: application/json" \
-		--header "authorization: Bearer $params" \
-		--data '{
-			"code": "'${1:-1}'"
-		}'
+    _post "getPasswords" \
+        "{\"code\":\"${1:-1}\"}"
 }
 
+# 1 - password: (string): <password>
+# 2 - name: (string): <name>
 function add_password() {
-	# 1 - password: (string): <password>
-	# 2 - name: (string): <name>
-	curl --request POST \
-		--url "$api/addPassword" \
-		--user-agent "$user_agent" \
-		--header "content-type: application/json" \
-		--header "authorization: Bearer $params" \
-		--data '{
-			"password": "'$1'",
-			"name": "'$2'"
-		}'
+    _post "addPassword" \
+        "{\"password\":\"$1\",\"name\":\"$2\"}"
 }
